b30f846ec8a5c688d4134da55b35addb347f5525
rendercold007 <flameboy568@gmail.com>
2025-08-03 10:55:58 +0530

    2nd commit

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const buttontd1 = document.queryselector('.buttoninput');
 const listtd1 = document.querySelector('.todolist');
 
 
-document.addEventListener('DOMContentLoaded', loadToDoList)
+document.addEventListener('DOMContentLoaded', loadToDoList);
+
+buttond1.addEventListener('click', clickButton);
+listtd1.addEventListener('click', okdel);
