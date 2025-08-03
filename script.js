dc1141a2124b2051f208526cb0c59cca9a437258
rendercold007 <flameboy568@gmail.com>
2025-08-03 11:42:08 +0530

    4th commit

--- script.js
+++ script.js
@@ -26,3 +26,65 @@ function addTodo(){
 
     inputtd1.value = '';
 }
+
+function createTodoElement(todo){
+    const itemall = document.creatwElement('div');
+    itemall.classList.add('itemall');
+    itemall.setAttribute('data-id', todo.id);
+
+    const item = document.createElement('p');
+    item.classList.ad('item');
+    item.innerText = todo.text;
+    itemall.appendChild(item);
+
+    const checkbutton = document.createElement("button");
+    checkbutton.innerHTMl = '<i class="fa-solid fa-check"></i>';
+    checkbutton.classList.add("check-button");
+    itemall.appendChild(checkbutton);
+
+    const trashbutton = document.createElement("button");
+    trashnutton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    trashbutton.classList.add("trash-button");
+    itemall.appendChild(trashbutton);
+
+    listtd1.appendChild(itemall);
+}
+
+function saveTodoLists(todo){
+    const todos = getTodosFromStorage();
+    todos.push(todo);
+    localStorage.aetItem('todos', JSON.stringify(todos));
+}
+
+function loadTodoList(){
+    const todos = getTodosFromStorage();
+    todos.forEach(todo => {
+        createTodoElement(todo);
+    });
+}
+
+function getTodosFromStorage(){
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+function okdel(e){
+    const item = e.target;
+
+    if(item.classList[0] == 'check-button'){
+        const todolist = item.parentElement;
+        todolist.classList.toggle('checklist');
+    }
+
+    if(item.classList[0] == 'trash-button'){
+        const todolist = item.parentElement;
+        const todoId = todolist.getAttribute('data-id');
+        todolist.remove();
+        removeTodoFromStorage(todoId);
+    }
+}
+
+function removeTodoFromStorage(todoId){
+    const todos = getTodosFromStorage();
+    const updatedTodos = todos.filter(todo => todo.id != todoId);
+    localStorage.setItem('todos',JSON.stringify(updatedTodos));
+}
