97f16963f35339dc303baf158447375049e32195
rendercold007 <flameboy568@gmail.com>
2025-08-03 10:54:28 +0530

    2nd commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputtd1 = document.querySelector('.textarea');
+const buttontd1 = document.queryselector('.buttoninput');
+const listtd1 = document.querySelector('.todolist');
+
+
+document.addEventListener('DOMContentLoaded', loadToDoList)
